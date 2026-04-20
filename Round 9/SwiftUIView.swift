57e308bf229f0f9972a636edddeb85dd12d71c40
BT_C09_16 <mobileappdev@bths.edu>
2026-04-20 13:41:38 -0400

    Added new view

---
+++ Round 9/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Round 9
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Trees")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
