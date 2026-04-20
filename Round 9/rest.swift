8c1db4b1200e41450a38c0eb962acde66389cf2c
BT_C09_16 <mobileappdev@bths.edu>
2026-04-20 13:45:17 -0400

    Added "rest" file

---
+++ Round 9/rest.swift
@@ -0,0 +1,18 @@
+//
+//  rest.swift
+//  Round 9
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct rest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    rest()
+}
