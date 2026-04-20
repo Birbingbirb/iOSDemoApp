1737dd101a2dcbbb23ee6e0b04ad7167267d8641
BT_C09_16 <mobileappdev@bths.edu>
2026-04-20 13:39:14 -0400

    Update img and txt

--- Round 9/ContentView.swift
+++ Round 9/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "up_arrow")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Yippie!!")
+            Text("Yipppie!!")
+            Text("Yippppie!!")
         }
         .padding()
     }
